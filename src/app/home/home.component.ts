d757fb19966bdbdd519b2a58b588f0cf2536139d
Dustin-Mikusko <dustin.mikusko@gmail.com>
2020-12-08 21:40:00 -0700

    Refactor onInit in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,19 +25,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
-
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
+    const courses = this.coursesService.loadAllCourses();
   }
 
   editCourse(course: Course) {
